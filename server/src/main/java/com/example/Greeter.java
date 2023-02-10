ab9aa1a00a6fb62d4bbcab38c18120c871a3e7db
Timiz Depot <timizdepot@gmail.com>
2023-02-10 00:20:24 -0500

    Updated the greeter.java file

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -9,13 +9,14 @@ public class Greeter {
    * This is a constructor.
    */
   public Greeter() {
-    return String("Hello Dff");
+
   }
 
  /**
    * This is a method.
    */
   public final String greet(final String someone) {
+    
     return String.format("Hello Dff, %s!", someone);
   }
 }
